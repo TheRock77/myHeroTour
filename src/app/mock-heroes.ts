eda6f879abee6ac16d8cdd382a36ff95364b258e
boni <boni@mailbox.org>
2023-12-28 21:33:38 +0100

    tutorial after part 4 v2

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,13 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 12, name: 'Dr. Nice' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr. IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 12, name: 'Rondrigo', initiative: 10 },
+  { id: 13, name: 'Galjan', initiative: 9 },
+  { id: 14, name: 'Ruriel', initiative: 18 },
+  { id: 15, name: 'Said', initiative: 12 },
+  { id: 16, name: 'Diego', initiative: 15 },
+  { id: 17, name: 'Angrimm', initiative: 17 },
+  { id: 18, name: 'Andalie', initiative: 12 },
+  { id: 19, name: 'Magma', initiative: 15 },
+  { id: 20, name: 'Tornado', initiative: 14 }
 ];
